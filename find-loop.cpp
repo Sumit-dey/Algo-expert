fba33d5ad92b3f2c1eec6400bf128dc272f71090
Sumit-dey <deysumit417@gmail.com>
2022-05-13 14:39:05 -0400

    Aa

---
+++ find-loop.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class LinkedList {
+public:
+  int value;
+  LinkedList *next;
+
+  LinkedList(int value);
+};
+
+LinkedList *findLoop(LinkedList *head) {
+  // Write your code here.
+  return nullptr;
+}
+
