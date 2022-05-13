4f3978e5b00dfac21891170ac5459d92d5ec4ae9
Sumit-dey <deysumit417@gmail.com>
2022-05-13 14:36:55 -0400

    aa

---
+++ sum-of-linked-list.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+// This is an input struct. Do not edit.
+class LinkedList {
+public:
+  int value;
+  LinkedList *next = nullptr;
+
+  LinkedList(int value) { this->value = value; }
+};
+
+LinkedList *sumOfLinkedLists(LinkedList *linkedListOne,
+                             LinkedList *linkedListTwo) {
+
+  return nullptr;
+}
